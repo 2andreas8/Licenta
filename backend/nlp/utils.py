818a752e72a0fed67725d7574b06ce9e5255ccfa
2andreas8 <andreas.faur@student.unitbv.ro>
2025-06-02 11:00:53 +0300

    Some tests

--- backend/nlp/utils.py
+++ backend/nlp/utils.py
@@ -27,7 +27,7 @@ def generate_answer(question: str, context: str)-> str:
     # TODO - Implement the logic to generate an answer based on the question and context
 
     template = """Use the following pieces of context to answer the question at the end. 
-    If you don't know the answer, just say “Sorry, con’t answer your question, try to ask it in a different way”, don't try to make up an answer. 
+    If you don't know the answer, just say “Sorry, can’t answer your question, try to ask it in a different way”, don't try to make up an answer. 
     Use the only the following pieces of context, don't use your own knowledge. 
     
     Context: {context}
